f8a14448ace2a77190d8097a6ffd0463e39cb9f4
Merv Antonio <mervantonio@gmail.com>
2018-03-25 18:24:01 -0400

    edit quotes

--- clproject.js
+++ clproject.js
@@ -5,16 +5,16 @@
         var el = document.getElementById("quotes");
         
         var quotes = [
-      ["Travel makes one modest. You see what a tiny place you occupy in the world.", "--Gustav Flaubert"],
-	    ["The gladdest moment in human life, me thinks, is a departure into unknown lands.", "--Sir Richard Burton"],	
-	    ["Traveling – it leaves you speechless, then turns you into a storyteller.", "--Ibn Battuta"],
-	    ["Like all great travelers, I have seen more than I remember, and remember more than I have seen.", "--Benjamin Disraeli"],	
-	    ["Take only memories, leave only footprints.", "--Chief Seattle"], 
-      ["A great way to learn about your country is to leave it.", "--Henry Rollins"], 	
-	    ["Travel is never a matter of money but of courage.", "--Paolo Coelho"], 	
-      ["Travel far enough, you meet yourself.", "--David Mitchell"], 
-      ["Life is either a daring adventure or nothing at all.", "--Helen Keller"],
-      ["Life shrinks or expands in proportion to one’s courage.", "--Anais Nin"], 
+      ["Travel makes one modest. You see what a tiny place you occupy in the world.", "-Gustav Flaubert"],
+	    ["The gladdest moment in human life, me thinks, is a departure into unknown lands.", "-Sir Richard Burton"],	
+	    ["Traveling – it leaves you speechless, then turns you into a storyteller.", "-Ibn Battuta"],
+	    ["Like all great travelers, I have seen more than I remember, and remember more than I have seen.", "-Benjamin Disraeli"],	
+	    ["Take only memories, leave only footprints.", "-Chief Seattle"], 
+      ["A great way to learn about your country is to leave it.", "-Henry Rollins"], 	
+	    ["Travel is never a matter of money but of courage.", "-Paolo Coelho"], 	
+      ["Travel far enough, you meet yourself.", "-David Mitchell"], 
+      ["Life is either a daring adventure or nothing at all.", "-Helen Keller"],
+      ["Life shrinks or expands in proportion to one’s courage.", "-Anais Nin"], 
 
     ];
 
